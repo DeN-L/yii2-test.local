187ba519057d3c69e2ad33a694cacc373e50aa4c
Denis Lyubchak <lyub4ak@gmail.com>
2020-05-09 03:36:50 +0300

    Create user migration.

---
+++ migrations/m200508_234207_create_user.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200508_234207_create_user
+ */
+class m200508_234207_create_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->insert('user', [
+            'username' => 'Jack Doe',
+            'auth_key' => 'TzRTagShVI6cavMqPZRrxSe5TUKA6_O4',
+            'password' => '1111',
+            'password_reset_token' => null,
+            'email' => 'jack-doe@milo.ru',
+            'status' => 10,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+
+}
